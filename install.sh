5d9930d8957c8d238ac38ec2d788e473739c0c72
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-12-07 16:12:26 +0800

    Added initial installation of tools
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,66 @@
 #!/bin/sh
 
-
+# Update the remote repo
 sudo xbps-install -Syuv
+
+
+# Install primary tools
+sudo xbps-install -Syuv \
+	base-devel \
+	xorg-minimal \
+	xorg-fonts \
+	bspwm \
+	sxhkd \
+	xterm \
+	xset \
+	xset \
+	pfetch \
+	xdo \
+	ffmpeg \
+	mpc \
+	mpv \
+	mpd \
+	ncmpcpp \
+	autox \
+	git \
+	gnupg \
+	git-lfs \
+	curl \
+	xtitle \
+	xclip \
+	libXft-devel \
+	libXinerama-devel \
+	xrdb \
+	xprop \
+	xdg-utils \
+	xprop \
+	tlp \
+	xf86-input-synaptics \
+	xf86-input-evdev \
+	alsa-utils \
+	wireless_tools \
+	ntfs-3g \
+	hsetroot \
+	openntpd \
+	xset \
+	xbacklight \
+	fontconfig-devel \
+	dunst \
+	bash \
+	bash-completion \
+	coreutils \
+	moreutils \
+	find-utils \
+	binutils \
+	aircrack-ng \
+	binwalk \
+	the_silver_searcher \
+	ImageMagick \
+	tree \
+	netpbm \
+	grep \
+	openssh \
+	screen
+
+
+
